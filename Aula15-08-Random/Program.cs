51c1b3e4400f80f0f3698bcc9f9917a66ff31ed7
t-305498 <t-305498@SOR0539873W10-1>
2022-08-15 21:23:43 -0300

    Adicionar arquivos de projeto.

---
+++ Aula15-08-Random/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula15_08_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            Console.WriteLine("Teste 1:  " +  r.Next());
+            Console.WriteLine("Teste 2:  " +  r.Next(10));
+            Console.WriteLine("Teste 3:  " + r.Next(10,21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+            Console.WriteLine("Teste 3:  " + r.Next(10, 21));
+        }
+    }
+}
+
---
+++ Aula15-08-variaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula15_08_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            double y = 10.5;
+            double z = 10;
+            string name ="Senac";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(name);
+
+            Console.WriteLine(x + y + z + name);
+            Console.WriteLine(name + x + y + z);
+            Console.WriteLine(1 + 2 + 3 + name + 1 + 2 + 3);
+            Console.WriteLine(1 + 2 + 3 + name + (1 + 2 + 3));
+
+        }
+    }
+}
