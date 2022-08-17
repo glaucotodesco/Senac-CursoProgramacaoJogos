0862ef6746058c77dee577a30625b7c7714f0a84
Glauco Todesco <glauco.todesco@abutua.com>
2022-08-16 22:32:35 -0300

    Exercicio2

---
+++ Aula16-08-Exercicio02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula16_08_Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.WriteLine("Digite um valor inteiro: ");
+            valor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Antecessor: " + (valor - 1  ));
+            Console.WriteLine("Sucessor..: " + (valor + 1  ));
+            Console.WriteLine("Dobro.....: " + (valor * 2  ));
+            Console.WriteLine("Metade....: " + (valor / 2.0));
+        }
+    }
+}
