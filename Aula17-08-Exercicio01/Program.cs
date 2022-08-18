6cc66f3c6bda8064d5002002ae4129bad025d1d5
Glauco Todesco <glauco.todesco@abutua.com>
2022-08-18 19:56:55 -0300

    Exercicio 17-08

--- Aula17-08-Exercicio01/Program.cs
+++ Aula17-08-Exercicio01/Program.cs
@@ -6,21 +6,31 @@ namespace Aula17_08_Exercicio01
     {
         static void Main(string[] args)
         {
-            int n1, n2;
+            int n1, n2, n3;
 
-            Console.Write("Digite um valor inteiro: ");
+            Console.Write("Digite um valor inteiroa: ");
             n1 = int.Parse(Console.ReadLine());
 
             Console.Write("Digite um valor inteiro: ");
             n2 = int.Parse(Console.ReadLine());
 
-            
-            if(n1 > n2)
+            Console.Write("Digite um valor inteiro: ");
+            n3 = int.Parse(Console.ReadLine());
+
+
+            if (n1 > n2   &&  n1 > n3)
             {
                 Console.WriteLine(n1);
             }
             else{
-                Console.WriteLine(n2);
+                if(n2 > n3)
+                {
+                    Console.WriteLine(n2);
+                }
+                else
+                {
+                    Console.WriteLine(n3);
+                }
             }
 
 
