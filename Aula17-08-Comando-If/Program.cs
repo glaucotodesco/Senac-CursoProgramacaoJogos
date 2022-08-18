122bac33acbd8aabfa39d08c6292376f8317f875
Glauco Todesco <glauco.todesco@abutua.com>
2022-08-17 22:36:40 -0300

    Exercicios

---
+++ Aula17-08-Comando-If/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Aula17_08_Comando_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val;
+
+            Console.Write("Digite um valor: ");
+            val = int.Parse(Console.ReadLine());
+
+            if(val > 10)
+            {
+                Console.WriteLine("A");
+            }
+
+            if (val > 10 && val < 20)
+            {
+                Console.WriteLine("B");
+            }
+
+            if (val > 10 || val < 20)
+            {
+                Console.WriteLine("C");
+            }
+
+            if (val < 10 || val > 20)
+            {
+                Console.WriteLine("D");
+            }
+
+
+            if (val >= 10)
+            {
+                Console.WriteLine("E");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+
+
+
+            Console.WriteLine("FIM!!!!");
+
+
+
+        }
+    }
+}
---
+++ Aula17-08-Exercicio01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aula17_08_Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.Write("Digite um valor inteiro: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um valor inteiro: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            
+            if(n1 > n2)
+            {
+                Console.WriteLine(n1);
+            }
+            else{
+                Console.WriteLine(n2);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
