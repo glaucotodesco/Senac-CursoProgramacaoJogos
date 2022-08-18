12a8e47be9a6dcc04d1f4aa0c9d652569fed9a17
Glauco Todesco <glauco.todesco@abutua.com>
2022-08-18 20:28:28 -0300

    Exercicio2

---
+++ Aula18-08-Exercicio2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+/*
+ Exercício 2:
+  Sortear um valor inteiro de 1 à 10.
+  O usuário terá 3 chances de acertar o valor sorteado.
+  Mostrar na tela se o usuário acertou ou errou. Se errou mostrar o valor
+*/
+
+namespace Aula18_08_Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sorteio, numero;
+
+            Random r = new Random();
+            sorteio = r.Next(1, 11);
+
+            //Console.WriteLine("Numero sorteado = " + sorteio);
+
+            Console.WriteLine("Digite a sua chance 1: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero == sorteio)
+            {
+                Console.WriteLine("Parabéns! Você acertou de primeira!");
+            }
+            else
+            {
+                Console.WriteLine("Digite a sua chance 2: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero == sorteio)
+                {
+                    Console.WriteLine("Parabéns! Você acertou!");
+                }
+                else
+                {
+                    Console.WriteLine("Digite a sua chance 3: ");
+                    numero = int.Parse(Console.ReadLine());
+
+                    if (numero == sorteio)
+                    {
+                        Console.WriteLine("Parabéns! Você acertou!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Você não acertou! Número sorteado = " + sorteio);
+                    }
+                }
+
+            }
+
+
+
+        }
+    }
+}
