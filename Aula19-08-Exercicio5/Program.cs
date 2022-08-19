776d7d114ba5df2544fb2617aa5ded8c14def479
Glauco Todesco <glauco.todesco@abutua.com>
2022-08-19 19:57:17 -0300

    Aula 19-08

---
+++ Aula19-08-Exercicio5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula19_08_Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "SenAc";
+
+            Console.WriteLine(nome.Equals("SENAC"));
+            Console.WriteLine(nome.ToUpper().Equals("SENAC"));
+            Console.WriteLine(nome.Contains("A"));
+            Console.WriteLine(nome.ToUpper().Contains("A"));
+            Console.WriteLine(nome.IndexOf("A"));
+            Console.WriteLine(nome.ToUpper().IndexOf("A"));
+
+            string letra = Console.ReadLine();
+            if (nome.ToUpper().Contains(letra))
+            {
+                Console.WriteLine("Tem a letra na posição" +
+                    (nome.ToUpper().IndexOf(letra)+1)
+                    );
+            }
+
+        }
+    }
+}
