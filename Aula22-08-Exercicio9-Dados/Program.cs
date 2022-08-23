8e3a2e34598cc9e5df2115f732541190dbc8c190
Glauco Todesco <glauco.todesco@abutua.com>
2022-08-22 22:15:59 -0300

    ExercicioDados

---
+++ Aula22-08-Exercicio9-Dados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * Jogo dos dados
+ * Você vs Computador
+ * Lançar 2 dados por turno.
+ * Verificar quem é o vencedor ou se deu empate.
+ * Ao final de 10 turnos mostrar:
+ * 
+ * Total de vitórias do Jogador
+ * Total de vitórias do Computador
+ * Total de empates
+ * 
+ * Mostrar em venceu ou deu empate após 10 turnos.
+ * 
+ * 
+ */
+
+
+
+namespace Aula22_08_Exercicio9_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Aula22-08-Repeticao01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula22_08_Repeticao01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int i = 0, j;
+        
+            while(i < 15)
+            {
+                Console.WriteLine("Hello World! ");
+                i++;
+            }
+
+            Console.WriteLine("Digite o valor de j: ");
+            j = int.Parse(Console.ReadLine());
+            i = 0;
+            while (i < j)
+            {
+                Console.WriteLine("Só tem louco!!!! ");
+                i++;
+            }
+
+
+        }
+    }
+}
