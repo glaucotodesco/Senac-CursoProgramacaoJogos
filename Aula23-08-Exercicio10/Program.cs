2691581d6f1bbe06d242cfb7622f12c111c42032
Glauco Todesco <glauco.todesco@gmail.com>
2022-08-24 22:23:45 -0300

    Exercicio10 com métodos

--- Aula23-08-Exercicio10/Program.cs
+++ Aula23-08-Exercicio10/Program.cs
@@ -27,58 +27,112 @@ namespace Aula23_08_Exercicio10
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Random r = new Random();
-            int posicao = 1, dado=0, desenha;
-            
-          
+         
+            int posicao = 1, dado=0; 
+            string history="1 ";
+
+            DesenhaHUD(dado, posicao, history);
+            DesenhaTabuleiro(posicao);
 
             while (posicao != 20)
             {
+                dado = SorteiaDado();
                 Console.Clear();
-
-                if (dado != 0)
-                {
-                    Console.WriteLine("Dado: " + dado);
-                    Console.WriteLine("Posição: " + posicao);
-                }
-                else
-                {
-                    Console.WriteLine("Dado:");
-                    Console.WriteLine("Posição: " + posicao );
-                }
-
-                desenha = 1;
-                
-                while (desenha <= 20)
-                {
-
-                    if (desenha == posicao)
-                    {
-                        Console.Write("[X]");
-                    }
-                    else
-                    {
-                        Console.Write("[ ]");
-                    }
-                    desenha++;
-                }
-
-                Console.WriteLine("\n\nTecle ENTER para lançar o dado!");
-                Console.ReadKey();
-                dado = r.Next(1, 7);
                 posicao += dado;
+                posicao = VerificaPosicao(posicao);
+                posicao = VerificaSorteOuAzar(posicao);
+                history += posicao + " ";
+                DesenhaHUD(dado, posicao, history);
+                DesenhaTabuleiro(posicao);
                 
-                if (posicao > 20)
+               
+
+            }
+
+            DesenhaFimDeJogo(posicao);
+        }
+
+
+        static void DesenhaFimDeJogo(int posicao)
+        {
+            Console.Clear();
+            Console.WriteLine("\nVocê venceu!");
+            DesenhaTabuleiro(posicao);
+            Console.WriteLine("\nFIM");
+        }
+
+        static int VerificaSorteOuAzar(int posicao)
+        {
+            string msg="";
+            if (posicao == 13 || posicao == 19)
+            {
+                msg = "Azar: Você caiu na posicao " + posicao + " teve que voltar para a posição 1!";
+                posicao = 1;
+            }
+            else
+            {
+                if (posicao == 4 || posicao == 9)
                 {
-                    posicao = 20  - (posicao - 20);
+                    msg="Sorte: Você caiu na posicao " + posicao + " e avançou para a casa 18!";
+                    posicao = 18;
+                    
                 }
             }
 
-            Console.Clear();
-            Console.WriteLine("Você venceu!");
-            desenha = 1;
+            if (msg.Length > 0)
+            {
+                Console.WriteLine(msg);
+            }
+
+            return posicao;
+
+        }
+
+
+        static int VerificaPosicao(int posicao)
+        {
+            if (posicao > 20)
+            {
+                posicao = 20 - (posicao - 20);
+            }
+
+            return posicao;
+        }
+
+        static int SorteiaDado()
+        {
+            int dado;
+            Random r = new Random();
+            Console.WriteLine("\n\nTecle ENTER para lançar o dado!");
+            Console.ReadKey();
+            dado =  r.Next(1, 7);
+            return dado;
+        }
+
+        static void DesenhaHUD(int dado, int posicao, string history)
+        {
+            
+            if (dado != 0)
+            {
+                Console.WriteLine("Dado: " + dado);
+                Console.WriteLine("Posição: " + posicao);
+            }
+            else
+            {
+                Console.WriteLine("Dado:");
+                Console.WriteLine("Posição: " + posicao);
+            }
+            Console.WriteLine("Histórico de posições: " + history);
+        }
+
+        static void DesenhaTabuleiro(int posicao)
+        {
+            int desenha = 1;
+            Console.WriteLine("");
+            Console.Write("Tabuleiro: ");
             while (desenha <= 20)
             {
 
@@ -92,8 +146,8 @@ namespace Aula23_08_Exercicio10
                 }
                 desenha++;
             }
-            Console.WriteLine("\nFIM");
         }
+
 
     }
     
