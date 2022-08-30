73e995eaf0b9585590c31117b15bfbda220c1712
glaucotodesco <glauco.todesco@gmail.com>
2022-08-30 19:49:45 -0300

    Projeto21

---
+++ Aula30-08-Jogo21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula30_08_Jogo21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
