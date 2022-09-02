6f9a4eae269eaac79689cb6552686c504fee2fab
229799 284582 <t-284582@senacsp.edu.br>
2022-09-02 20:56:27 -0300

    atributo

---
+++ Aula02-09-Atributos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula02_09_Atributos
+{
+    class Program
+    {
+        
+        //Atributo
+        static int x=100;
+
+        static void Main(string[] args)
+        {
+            int y = 1; 
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+            M1();
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+            M2(y);
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+
+        }
+
+        static void M2(int y)
+        {
+            y = 20;
+            Console.WriteLine(y);
+        }
+        static void M1()
+        {
+            int y = 10;
+            Console.WriteLine(y);
+        }
+    }
+}
