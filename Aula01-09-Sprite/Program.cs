65d3349e8f77edee9215d1a646cb2d4a0e6163ba
glaucotodesco <glauco.todesco@gmail.com>
2022-09-01 22:33:02 -0300

    sprite

---
+++ Aula01-09-Sprite/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+namespace Aula01_09_Sprite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x, i,j;
+            string pos;
+            Random r = new Random();
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+
+                i = 0;
+                j = r.Next(1, 10);
+                pos = "";
+                while (i < j){
+                    pos += "\t";
+                    i++;
+                }
+
+                x = 20;
+                while(x > 0)
+                {
+                    i = 0;
+                    Console.Clear();
+
+                    
+                    while (i < x)
+                    {
+                        Console.WriteLine("");
+                        i++;
+                    }
+
+                    Console.WriteLine(pos + "   /\\   ");
+                    Console.WriteLine(pos + "   ||    ");
+                    Console.WriteLine(pos + "   ||    ");
+                    Console.WriteLine(pos + "  /||\\  ");
+                    x--;
+                    Thread.Sleep(10);
+                }
+                
+            }
+
+        }
+    }
+}
