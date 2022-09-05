bab056e4bdb120c512fd0e5597ccc3a47ae129f0
229799 284582 <t-284582@senacsp.edu.br>
2022-09-05 20:33:55 -0300

    Aula 05/09

---
+++ Aula05-09-Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Aula05_09_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dados = new int[10];
+            int maior;
+
+            for(int i=0; i < dados.Length; i++) 
+            {
+                Console.Write("Digite um valor: ");
+                dados[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach(int v in dados)
+            {
+                Console.WriteLine(v);
+            }
+
+            maior = dados[0];
+            foreach (int v in dados)
+            {
+                if(maior < v)
+                {
+                    maior = v;
+                }
+            }
+            Console.WriteLine("Maior valor = " + maior);
+
+            Console.Write("Posição =");
+            for (int i = 0; i < dados.Length; i++)
+            {
+                if(dados[i] == maior)
+                {
+                    Console.Write(" " +( i + 1));
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Aula05-09-PlayerVsPC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula05_09_PlayerVsPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int turnoPlayer;
+
+            turnoPlayer = r.Next(3);
+            
+            Console.WriteLine("Quem começa?");
+            if (turnoPlayer == 1)
+            {
+                Console.WriteLine("Player");
+            }
+            else
+            {
+                Console.WriteLine("PC");
+            }
+
+
+        }
+    }
+}
